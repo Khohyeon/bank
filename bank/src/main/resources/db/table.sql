2e527b2eb035151366b2b392acae239ede1e6bf8
kim ho hyeon <khh5762@naver.com>
2023-02-15 11:04:57 +0900

    table 생성완료

--- bank/src/main/resources/db/table.sql
+++ bank/src/main/resources/db/table.sql
@@ -2,7 +2,14 @@ create table user_tb(
     id int auto_increment primary key,
     username varchar not null unique,
     password varchar not null,
-    email varchar not null,
-    profile varchar,
+    fullname varchar not null,
+    created_at timestamp 
+);
+create table account_tb(
+    id int auto_increment primary key,
+    number varchar not null unique,
+    password varchar not null,
+    balance bigint not null,
+    userId int not null,
     created_at timestamp 
 );
