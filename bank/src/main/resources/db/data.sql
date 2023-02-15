8639f216366a2e9ad77b68b6bc1db3602b091ec5
kim ho hyeon <khh5762@naver.com>
2023-02-15 09:56:17 +0900

    초반 세팅완료

---
+++ bank/src/main/resources/db/data.sql
@@ -0,0 +1,4 @@
+insert into user_tb(username, password, email, profile, created_at) values('ssar', '1234', 'ssar@nate.com', '/images/profile.png', now());
+insert into user_tb(username, password, email, profile, created_at) values('love', '1234', 'love@nate.com', '/images/profile.png', now());
+
+commit;
